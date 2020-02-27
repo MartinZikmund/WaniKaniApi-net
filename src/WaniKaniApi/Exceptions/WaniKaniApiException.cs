b5f8ffe5136059b59007915045ee61f2f04a46f8
Martin Zikmund <martinzikmund@live.com>
2020-02-27 22:28:45 +0100

    Started building API

---
+++ src/WaniKaniApi/Exceptions/WaniKaniApiException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaniKaniApi.Exceptions
+{
+    [Serializable]
+    public class WaniKaniApiException : Exception
+    {
+        public WaniKaniApiException()
+        {
+        }
+
+        public WaniKaniApiException(string message) : base(message)
+        {
+        }
+
+        public WaniKaniApiException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected WaniKaniApiException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ src/WaniKaniApi/WaniKaniClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaniKaniApi
+{
+    public class WaniKaniClient : IWaniKaniClient
+    {
+        private readonly string _apiKey;
+
+        public WaniKaniClient(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+    }
+}
